318629a17a6c22484af81207f2866ea0dece4a81
syjn99 <syjn99@gmail.com>
2025-01-08 13:32:21 +0900

    Fix reward/penalty weights

--- src/duty/attestation/attestation.constants.ts
+++ src/duty/attestation/attestation.constants.ts
@@ -1,7 +1,10 @@
 // from https://eth2book.info/bellatrix/part2/incentives/rewards/
-export const TIMELY_SOURCE_WEIGHT = 14; // Ws
-export const TIMELY_TARGET_WEIGHT = 26; // Wt
-export const TIMELY_HEAD_WEIGHT = 14; // Wh
+// and modified for Over Protocol.
+// See https://github.com/overprotocol/chronos/blob/0414ea20f92807b810b1ca980c14985ac5cf6960/config/params/mainnet_config.go#L215-L221
+const LIGHT_LAYER_WEIGHT = 8;
+export const TIMELY_SOURCE_WEIGHT = 12; // Ws
+export const TIMELY_TARGET_WEIGHT = 24; // Wt
+export const TIMELY_HEAD_WEIGHT = 12 + LIGHT_LAYER_WEIGHT; // Wh
 const WEIGHT_DENOMINATOR = 64; // W sigma
 
 const timelySource = (attIncDelay: number, attValidSource: boolean): boolean => {
