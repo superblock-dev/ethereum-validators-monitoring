f500dd7bff66d756a312ab5d7bf08979bf01ea29
syjn99 <syjn99@gmail.com>
2025-01-08 14:42:31 +0900

    Fix proposer reward constant

--- src/duty/propose/propose.constants.ts
+++ src/duty/propose/propose.constants.ts
@@ -1,10 +1,11 @@
 // from https://eth2book.info/bellatrix/part2/incentives/rewards/
 
+const LIGHT_LAYER_WEIGHT = 8;
 export const PROPOSER_WEIGHT = 8; // Wp
 export const WEIGHT_DENOMINATOR = 64; // W sigma
 
 export const proposerAttPartReward = (
   r: number, // total rewards to the attesters in this block
 ) => {
-  return r / (((WEIGHT_DENOMINATOR - PROPOSER_WEIGHT) * WEIGHT_DENOMINATOR) / PROPOSER_WEIGHT);
+  return r / (((WEIGHT_DENOMINATOR - PROPOSER_WEIGHT - LIGHT_LAYER_WEIGHT) * WEIGHT_DENOMINATOR) / PROPOSER_WEIGHT);
 };
